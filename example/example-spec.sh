b45e5ead8b8a73c23b00ed6cf41d0731c1685622
d10n <david@bitinvert.com>
2020-07-23 00:58:16 -0400

    Add example spec files

---
+++ example/example-spec.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+cd "$(dirname "$0")" || exit 1
+. ../bashaspec.sh
+
+# Writing functions with :; at the end of
+# the opening line enables empty functions.
+test_empty() { :;
+}
+
+test_different_asserts() { :;
+  a=foo
+  b=bar
+  # Use [[ for assertions. See `man test` for usage details.
+  # Use return codes to identify assertion failures.
+  [[ "$a" = foo ]] || return 1
+  [[ "$b" = bar ]] || return 2
+  [[ "$a" = "$b" ]] || return 3
+  [[ "$a" != "$b" ]] || return 4
+}
+
+# Use () instead of {} to run the test in a subshell.
+# This can avoid side-effects from leaking to other tests.
+test_no_side_effect_bash_functions() ( :;
+  # The `exec` line below causes the subshell
+  # in this test to write its stderr to stdout
+  exec 2>&1
+  set -x
+  echo ok
+  false
+)
+
+test_false_assert_captures_stdout1() {
+  # Stdout is suppressed for passing tests, so it is fine to write logs.
+  echo foo
+  # false as the last statement of a test will fail the test;
+  # used here for demonstration
+  false
+}
+
+test_false_assert_captures_stdout2() {
+  echo bar
+  false
+}
---
+++ example/readme-spec.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+cd "$(dirname "$0")" || exit 1
+. ../bashaspec.sh
+test_concat() {
+  a='1'
+  b='2'
+  echo 'concatenating...'
+  c="$a$b"
+  [[ "$c" = "12" ]] || return 1
+  [[ "$c" != "ab" ]] || return 2
+  [[ "$c" -ne 3 ]] || return 3
+}
+test_add() {
+  a='1'
+  b='2'
+  echo 'adding...'
+  c=$(( a+b ))
+  [[ "$c" -eq 3 ]] || return 1
+  [[ "$c" != "12" ]] || return 2
+  [[ "$c" != "ab" ]] || return 2
+}
+test_an_error() {
+  echo 'returning nonzero is an error'
+  echo 'using return codes can identify the failing assertion'
+  [[ 'a' = 'a' ]] || return 1
+  [[ 'a' = 'b' ]] || return 2 # causes return 2!
+  [[ 'a' = 'c' ]] && return 3
+}
+test_a_success() {
+  now="$(date +%s)"
+  future=$((now+1))
+  (( now < future )) || return 1
+}
