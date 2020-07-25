0280c8b37864095d7d24680cd19abf8db5cc9b7e
d10n <david@bitinvert.com>
2020-07-25 03:41:40 -0400

    Explain bashaspec-non-tap vs bashaspec in comment

--- bashaspec-non-tap.sh
+++ bashaspec-non-tap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # bashaspec - MIT licensed. Copyright 2020 d10n. Feel free to copy around.
+# Non-TAP version with simpler but less featureful implementation.
 
 # Verbose? true: line per test; false: dot per test; default false
 [[ ${1:-} = -v || ${1:-} = --verbose ]] && verbose=1 || verbose=0
