1266a793220f5aca3221aa0de049a9d4ffa6eea0
d10n <david@bitinvert.com>
2020-07-25 17:49:53 -0400

    Refine hook logic

--- bashaspec.sh
+++ bashaspec.sh
@@ -25,7 +25,7 @@ run_test_functions() {
   functions="$(compgen -A function | grep '^test_')"
   echo "1..$(printf '%s\n' "$functions" | wc -l | sed 's/[^0-9]//g')"
   test_index=0; summary_code=0
-  run_fn before_all >&$test_w || ba_status=$?; bail_if_fail before_all ${ba_status:-0} "$(cat <&$test_r)"
+  run_fn before_all >&$test_w; ba_status=$?; bail_if_fail before_all $ba_status "$(cat <&$test_r)"
   while IFS= read -r fn; do
     status=; fail=; ((test_index += 1))
     run_fn before_each >&$test_w || { status=$?; fail="$fn before_each"; }
@@ -37,7 +37,7 @@ run_test_functions() {
     [[ -z "$fail" ]] || echo "# $fail returned $status"
     [[ -z "$fail" && "$verbose" -lt 2 ]] || [[ -z "$out" ]] || printf '%s\n' "$out" | sed 's/^/# /'
   done <<<"$functions"
-  run_fn after_all >&$test_w || aa_status=$?; bail_if_fail after_all ${aa_status:-0} "$(cat <&$test_r)"
+  run_fn after_all >&$test_w; aa_status=$?; bail_if_fail after_all $aa_status "$(cat <&$test_r)"
   return "$summary_code"
 }
 
