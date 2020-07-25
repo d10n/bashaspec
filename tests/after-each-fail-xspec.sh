b2aee37d8a7484ef05fdd90c6352b4df43b96753
d10n <david@bitinvert.com>
2020-07-25 03:43:10 -0400

    Add hook tests

---
+++ tests/after-each-fail-xspec.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+cd "$(dirname "$0")" || exit 1
+. ../bashaspec.sh
+
+before_all() {
+  echo 'inside before_all'
+  true
+}
+
+after_all() {
+  echo 'inside after_all'
+  true
+}
+
+before_each() {
+  echo 'inside before_each'
+  true
+}
+
+after_each() {
+  echo 'inside after_each'
+  false
+}
+
+test_1() {
+  echo 'inside test_1'
+  true
+}
+
+test_2() {
+  echo 'inside test_2'
+  true
+}
+
+test_3() {
+  echo 'inside test_3'
+  true
+}
---
+++ tests/hook-state-preserved-for-tests-spec.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+cd "$(dirname "$0")" || exit 1
+. ../bashaspec.sh
+
+before_all_run=0
+before_each_run=0
+after_each_run=0
+after_all_run=0
+
+before_all() {
+  before_all_run=1
+  pushd . # increase dir count to 2
+}
+after_all() {
+  after_all_run=1
+  popd # decrease dir count to 1
+}
+before_each() {
+  before_each_run=1
+  pushd . # increase dir count to 3
+}
+after_each() {
+  after_each_run=1
+  popd # decrease dir count to 2
+}
+
+test_1() {
+  (( before_all_run )) || return 1
+  (( before_each_run )) || return 2
+  ! (( after_each_run )) || return 4
+  ! (( after_all_run )) || return 3
+  dir_count="$(dirs -v | wc -l)"
+  echo "dir count $dir_count should be 3"
+  [[ "$dir_count" -eq 3 ]] || return 5
+}
+
+test_2() {
+  (( before_all_run )) || return 1
+  (( before_each_run )) || return 2
+  (( after_each_run )) || return 4 # after_each ran from previous test
+  ! (( after_all_run )) || return 3
+  # note: test order is not guaranteed; test_2 just runs after test_1 for me
+  dir_count="$(dirs -v | wc -l)"
+  echo "dir count $dir_count should be 3"
+  [[ "$dir_count" -eq 3 ]] || return 5
+}
